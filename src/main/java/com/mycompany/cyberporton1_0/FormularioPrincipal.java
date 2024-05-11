2c3daded700c4bb576433a8c06a4816c570363ae
ladoly <ladol@LAPTOP-GV1SQOCQ>
2024-05-11 11:15:50 -0600

    Se configuraron las rutas para poner imagenes (bastante dificil xd) y se diseño el inicio de la aplicacion

--- src/main/java/com/mycompany/cyberporton1_0/FormularioPrincipal.java
+++ src/main/java/com/mycompany/cyberporton1_0/FormularioPrincipal.java
@@ -1,22 +1,43 @@
 package com.mycompany.cyberporton1_0;
-import java.awt.Color;
+
 import com.mycompany.cyberporton1_0.Clases.MySqlConnection;
+import java.awt.Image;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.awt.Color;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 
 public class FormularioPrincipal extends javax.swing.JFrame {
     private MySqlConnection conexion;
     
     public FormularioPrincipal() {
         this.conexion = new MySqlConnection();
-        initComponents();       
+        initComponents();
         setLocationRelativeTo(null);
-        this.getContentPane().setBackground(new Color(220,223,241));
+        
+        ImagenHotSale();
+        ImagenCyberMonday();
+        this.getContentPane().setBackground(new Color(220, 223, 241));
     }
     
+    
+   public void ImagenHotSale() {
+        ImageIcon HotSale = new ImageIcon(getClass().getResource("/hotsale.jpg"));
+        Icon HotSaleIcon = new ImageIcon(HotSale.getImage().getScaledInstance(lblHotSale.getWidth(), lblHotSale.getHeight(), Image.SCALE_SMOOTH));
+        lblHotSale.setIcon(HotSaleIcon);
+    }
+     public void ImagenCyberMonday() {
+        ImageIcon CyberMonday = new ImageIcon(getClass().getResource("/cybermonday.jpg"));
+        Icon CyberMondayIcon = new ImageIcon(CyberMonday.getImage().getScaledInstance(lblCyberMonday.getWidth(), lblCyberMonday.getHeight(), Image.SCALE_SMOOTH));
+        lblCyberMonday.setIcon(CyberMondayIcon);
+    }
  
+
+ 
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -25,6 +46,12 @@ public class FormularioPrincipal extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel3 = new javax.swing.JPanel();
+        lblHotSale = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        lblCyberMonday = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
         jPanel4 = new javax.swing.JPanel();
         jPanel5 = new javax.swing.JPanel();
         jPanel6 = new javax.swing.JPanel();
@@ -47,7 +74,7 @@ public class FormularioPrincipal extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(23, 23, 23)
                 .addComponent(jLabel1)
-                .addContainerGap(505, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -57,15 +84,91 @@ public class FormularioPrincipal extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
+        lblHotSale.setToolTipText("");
+        lblHotSale.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lblHotSale.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblHotSaleMouseClicked(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("HP Simplified Hans", 3, 24)); // NOI18N
+        jLabel2.setText("Aprovecha nuestras ofertas de hot sale !");
+
+        jLabel3.setFont(new java.awt.Font("HP Simplified Hans", 3, 10)); // NOI18N
+        jLabel3.setText("valido del 15 al 17 de mayo de 2024");
+
+        lblCyberMonday.setToolTipText("");
+        lblCyberMonday.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lblCyberMonday.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseMoved(java.awt.event.MouseEvent evt) {
+                lblCyberMondayMouseMoved(evt);
+            }
+        });
+        lblCyberMonday.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblCyberMondayMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                lblCyberMondayMouseEntered(evt);
+            }
+        });
+
+        jLabel4.setFont(new java.awt.Font("HP Simplified Hans", 3, 12)); // NOI18N
+        jLabel4.setText("<html> <p>Que esperas para hacerlo!  Da click  en la imagen para visitar nuestras ofertas de cyber monday</p></html>");
+        jLabel4.setToolTipText("");
+
+        jLabel5.setFont(new java.awt.Font("HP Simplified Hans", 3, 24)); // NOI18N
+        jLabel5.setText("<html> <p>Quieres comprar una computadora?</p></html>");
+        jLabel5.setToolTipText("");
+
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 647, Short.MAX_VALUE)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(lblHotSale, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel3Layout.createSequentialGroup()
+                                .addGap(246, 246, 246)
+                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel3Layout.createSequentialGroup()
+                                .addGap(128, 128, 128)
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap(40, Short.MAX_VALUE)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(lblCyberMonday, javax.swing.GroupLayout.PREFERRED_SIZE, 413, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 401, Short.MAX_VALUE)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblHotSale, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(lblCyberMonday, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(78, 78, 78))))
         );
 
         jTabbedPane1.addTab("Inicio", jPanel3);
@@ -74,7 +177,7 @@ public class FormularioPrincipal extends javax.swing.JFrame {
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 647, Short.MAX_VALUE)
+            .addGap(0, 710, Short.MAX_VALUE)
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -87,7 +190,7 @@ public class FormularioPrincipal extends javax.swing.JFrame {
         jPanel5.setLayout(jPanel5Layout);
         jPanel5Layout.setHorizontalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 647, Short.MAX_VALUE)
+            .addGap(0, 710, Short.MAX_VALUE)
         );
         jPanel5Layout.setVerticalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -100,14 +203,14 @@ public class FormularioPrincipal extends javax.swing.JFrame {
         jPanel6.setLayout(jPanel6Layout);
         jPanel6Layout.setHorizontalGroup(
             jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 647, Short.MAX_VALUE)
+            .addGap(0, 710, Short.MAX_VALUE)
         );
         jPanel6Layout.setVerticalGroup(
             jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 401, Short.MAX_VALUE)
         );
 
-        jTabbedPane1.addTab("Iniciar sesión/Registrarse", jPanel6);
+        jTabbedPane1.addTab("Ventas", jPanel6);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -116,13 +219,13 @@ public class FormularioPrincipal extends javax.swing.JFrame {
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 652, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jTabbedPane1)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 428, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -130,6 +233,24 @@ public class FormularioPrincipal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void lblCyberMondayMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCyberMondayMouseClicked
+        int indiceTab = 1;  
+        jTabbedPane1.setSelectedIndex(indiceTab);
+    }//GEN-LAST:event_lblCyberMondayMouseClicked
+
+    private void lblCyberMondayMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCyberMondayMouseEntered
+       lblCyberMonday.setToolTipText("Haz clic para ir a seleccionar productos !");
+    }//GEN-LAST:event_lblCyberMondayMouseEntered
+
+    private void lblCyberMondayMouseMoved(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCyberMondayMouseMoved
+        lblCyberMonday.setToolTipText("Haz clic para ir a seleccionar productos !");
+    }//GEN-LAST:event_lblCyberMondayMouseMoved
+
+    private void lblHotSaleMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblHotSaleMouseClicked
+         int indiceTab = 1;  
+        jTabbedPane1.setSelectedIndex(indiceTab);
+    }//GEN-LAST:event_lblHotSaleMouseClicked
 
  
     public static void main(String args[]) {
@@ -168,11 +289,17 @@ public class FormularioPrincipal extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
     private javax.swing.JPanel jPanel6;
     private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JLabel lblCyberMonday;
+    private javax.swing.JLabel lblHotSale;
     // End of variables declaration//GEN-END:variables
 }
